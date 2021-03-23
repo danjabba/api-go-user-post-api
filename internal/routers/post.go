b6cadbf89ac706febf657c86207dee2dac69641f
Daniel Pacheco <danielpacheco@iMac-de-Daniel.local>
2021-03-23 10:41:19 -0500

    second commit

--- internal/routers/post.go
+++ internal/routers/post.go
@@ -8,7 +8,7 @@ import (
 )
 
 //  CreatePost ...
-func CreatePost(w http.ResponseWriter, r *http.Request) error {
+func CreatePost(w http.ResponseWriter, r *http.Request) {
 
 	var post models.Post
 
@@ -17,73 +17,71 @@ func CreatePost(w http.ResponseWriter, r *http.Request) error {
 	if err != nil {
 
 		http.Error(w, "Error"+err.Error(), 400)
-		return err
+
 	}
 
 	if len(post.Body) == 0 {
 		http.Error(w, "the body is required", 400)
-		return err
+
 	}
 
 	_, find, _ := tools.CheckPostAlreadyExist(post.Slug)
 	if find == true {
 		http.Error(w, "post already exist", 400)
-		return err
+
 	}
 
 	_, status, err := tools.InsertPost(post)
 
 	if err != nil {
 		http.Error(w, "error inserting new post"+err.Error(), 400)
-		return err
+
 	}
 
 	if status == false {
 		http.Error(w, "error inserting", 400)
-		return err
 
 	}
 
 	json.NewEncoder(w).Encode(&post)
 	w.WriteHeader(http.StatusCreated)
-	return nil
+
 }
 
 // GetPosts ...
-func GetPosts(w http.ResponseWriter, r *http.Request) error {
+func GetPosts(w http.ResponseWriter, r *http.Request) {
 
 	result, status := tools.GetAllPosts()
 	if status == false {
 		http.Error(w, "eror geting posts", http.StatusBadRequest)
-		return nil
+
 	}
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusAccepted)
 	json.NewEncoder(w).Encode(&result)
-	return nil
+
 }
 
 // GetOnePost ...
-func GetOnePost(w http.ResponseWriter, r *http.Request) error {
+func GetOnePost(w http.ResponseWriter, r *http.Request) {
 
 	ID := r.URL.Query().Get("id")
 
 	if len(ID) < 1 {
 		http.Error(w, "could send the id parameter", http.StatusBadRequest)
-		return nil
+
 	}
 	post, err := tools.GetPost(ID)
 
 	if err != nil {
 		http.Error(w, "error"+err.Error(), 400)
-		return err
 
 	}
 
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(post)
-	return nil
+
 }
 
 // UpdateOnePost ...
@@ -114,21 +112,21 @@ func UpdateOnePost(w http.ResponseWriter, r *http.Request) {
 }
 
 // DeleteOnePost ...
-func DeleteOnePost(w http.ResponseWriter, r *http.Request) error {
+func DeleteOnePost(w http.ResponseWriter, r *http.Request) {
 
 	ID := r.URL.Query().Get("id")
 	if len(ID) < 1 {
 		http.Error(w, "could send id parameter", http.StatusBadRequest)
-		return nil
+
 	}
 
 	err := tools.DeletePost(ID)
 	if err != nil {
 		http.Error(w, "error", http.StatusBadRequest)
-		return err
+
 	}
 
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	return nil
+
 }
--- internal/routers/user.go
+++ internal/routers/user.go
@@ -8,7 +8,7 @@ import (
 )
 
 // CreateUser ...
-func CreateUser(w http.ResponseWriter, r *http.Request) error {
+func CreateUser(w http.ResponseWriter, r *http.Request) {
 
 	var user models.User
 
@@ -17,79 +17,75 @@ func CreateUser(w http.ResponseWriter, r *http.Request) error {
 	if err != nil {
 
 		http.Error(w, "Error"+err.Error(), 400)
-		return err
+
 	}
 
 	if len(user.Email) == 0 {
 		http.Error(w, "the email is required", 400)
-		return err
+
 	}
 
 	_, find, _ := tools.CheckUserAlreadyExist(user.Email)
 	if find == true {
 		http.Error(w, "user already exist", 400)
-		return err
+
 	}
 
 	_, status, err := tools.InsertUser(user)
 
 	if err != nil {
 		http.Error(w, "error inserting new user"+err.Error(), 400)
-		return err
+
 	}
 
 	if status == false {
 		http.Error(w, "error inserting", 400)
-		return err
 
 	}
 
 	json.NewEncoder(w).Encode(&user)
 	w.WriteHeader(http.StatusCreated)
 
-	return nil
 }
 
 // GetUsers ...
-func GetUsers(w http.ResponseWriter, r *http.Request) error {
+func GetUsers(w http.ResponseWriter, r *http.Request) {
 
 	result, status := tools.GetAllUsers()
 	if status == false {
 		http.Error(w, "eror geting users", http.StatusBadRequest)
-		return nil
+
 	}
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusAccepted)
 	json.NewEncoder(w).Encode(&result)
 
-	return nil
 }
 
 // GetOneUser ...
-func GetOneUser(w http.ResponseWriter, r *http.Request) error {
+func GetOneUser(w http.ResponseWriter, r *http.Request) {
 
 	ID := r.URL.Query().Get("id")
 
 	if len(ID) < 1 {
 		http.Error(w, "could send the id parameter", http.StatusBadRequest)
-		return nil
+
 	}
 	user, err := tools.GetUser(ID)
 
 	if err != nil {
 		http.Error(w, "error"+err.Error(), 400)
-		return err
 
 	}
 
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(user)
-	return nil
+
 }
 
 // UpdateOneUser ...
-func UpdateOneUser(w http.ResponseWriter, r *http.Request) error {
+func UpdateOneUser(w http.ResponseWriter, r *http.Request) {
 	var user models.User
 	var ID string
 
@@ -97,41 +93,40 @@ func UpdateOneUser(w http.ResponseWriter, r *http.Request) error {
 
 	if err != nil {
 		http.Error(w, "data incorrect"+err.Error(), 400)
-		return err
+
 	}
 	var status bool
 	status, err = tools.UpdateUser(user, ID)
 	if err != nil {
 		http.Error(w, "error Updating user"+err.Error(), 400)
-		return err
+
 	}
 
 	if status == false {
 		http.Error(w, "error Updating the user"+err.Error(), 400)
-		return nil
+
 	}
 
 	w.WriteHeader(http.StatusAccepted)
-	return nil
 
 }
 
 // DeleteOneUser ...
-func DeleteOneUser(w http.ResponseWriter, r *http.Request) error {
+func DeleteOneUser(w http.ResponseWriter, r *http.Request) {
 
 	ID := r.URL.Query().Get("id")
 	if len(ID) < 1 {
 		http.Error(w, "could send id parameter", http.StatusBadRequest)
-		return nil
+
 	}
 
 	err := tools.DeleteUser(ID)
 	if err != nil {
 		http.Error(w, "error", http.StatusBadRequest)
-		return err
+
 	}
 
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	return err
+
 }
